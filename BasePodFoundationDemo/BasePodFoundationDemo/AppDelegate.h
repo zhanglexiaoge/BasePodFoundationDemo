2afa24d9c778b58082b4bbea43a8d83dc02f8091
zl <miugodigital@MiugodigitaldeMacBook-Air.local>
2018-05-18 11:25:35 +0800

    第一次提交

---
+++ BasePodFoundationDemo/BasePodFoundationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BasePodFoundationDemo
+//
+//  Created by Miugodigital on 2018/5/18.
+//  Copyright © 2018年 zhangle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BasePodFoundationDemo/BasePodFoundationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BasePodFoundationDemo
+//
+//  Created by Miugodigital on 2018/5/18.
+//  Copyright © 2018年 zhangle. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
